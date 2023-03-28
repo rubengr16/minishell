0627cb997ff7ee586dbfdf963ef62233dd45a553
rgallego <rgallego@student.42madrid.com>
2023-03-28 20:32:20 +0200

    almost nothing worth it

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 Install readline:  
 * Mac:  
 &emsp; Install brew:  
-&emsp; ```rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update```  
+&emsp; `rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && echo 'export PATH=$HOME/.brew/bin:$PATH' >> $HOME/.zshrc && source $HOME/.zshrc && brew update`  
 &emsp; Install Readline Library:  
-&emsp; ```brew install readline```  
+&emsp; `brew install readline`  
 
 
 ## Allowed functions
---
+++ vbles.mk
@@ -0,0 +1,37 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    vbles.mk                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rgallego <rgallego@student.42madrid.com    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/03/28 19:42:48 by rgallego          #+#    #+#              #
+#    Updated: 2023/03/28 20:25:33 by rgallego         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SRC_DIR	=			src/
+
+INC_DIR	=			inc/
+
+MINISHELL	=		main.c
+
+SRCS	= 			$(addprefix $(SRC_DIR), \
+					$(MINISHELL) \
+					)
+
+OBJS	=			$(SRCS:.c=.o)
+
+DPNDS	=			$(SRCS:.c=.d)
+
+CC		=			gcc
+
+RM		=			-rm -f
+
+CFLAGS	=			-Wall -Werror -Wextra -MD
+
+NAME	=			
+
+INC_ALL	=			\
+					$(INC_DIR) \
+					readline
