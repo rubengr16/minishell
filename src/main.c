ef55f7d0fa94a84885ff7578bf82c636f658ff78
rubengr16 <ruben16gallego@gmail.com>
2023-06-30 00:20:46 +0200

    solve empty line, pipe on end of line and add_history

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 18:48:06 by rgallego          #+#    #+#             */
-/*   Updated: 2023/06/29 21:01:47 by rgallego         ###   ########.fr       */
+/*   Updated: 2023/06/30 00:20:17 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,33 @@ void	discover_leaks(void)
 	system("leaks minishell");
 }
 
+char	*get_line(void)
+{
+	char	*result_line;
+	char	*line;
+	char	*aux;
+
+	line = readline("minishell> ");
+	if (line && *line && line[ft_strlen(line) - 1] == '|')
+	{
+		aux = NULL;
+		while (line[ft_strlen(line) - 1] == '|')
+		{
+			aux = readline("> ");
+			if (aux && *aux)
+			{
+				result_line = ft_strjoin(line, aux);
+				free(line);
+				line = result_line;
+			}
+			free(aux);
+			aux = NULL;
+		}
+	}
+	add_history(line);
+	return (line);
+}
+
 /*int argc, char **argv, char **env*/
 int	main(void)
 {
@@ -28,15 +55,18 @@ int	main(void)
 	t_token_list	*list;
 	t_cmd			*cmd_list;
 
-	line = readline("minishell> ");
-	list = tokenize(&line);
-	if (!list)
-		return (1);
-	print_list(list);
-	cmd_list = lexer(&list);
-	if (!cmd_list)
-		return (1);
-	print_cmd_list(cmd_list);
-	delete_cmd_list(&cmd_list);
+	line = get_line();
+	if (line && *line)
+	{
+		list = tokenize(&line);
+		if (!list)
+			return (1);
+		print_list(list);
+		cmd_list = lexer(&list);
+		if (!cmd_list)
+			return (1);
+		print_cmd_list(cmd_list);
+		delete_cmd_list(&cmd_list);
+	}
 	atexit(discover_leaks);
 }
