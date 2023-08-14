95a494723a8e26cc3a600790a2fed81f4fe10b6d
rgallego <ruben.gallegoramirezdeanton@telefonica.com>
2023-08-14 10:28:41 +0200

    solve mini_error of exit

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: rgallego <rgallego@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 18:22:20 by rgallego          #+#    #+#             */
-/*   Updated: 2023/08/14 01:52:58 by rgallego         ###   ########.fr       */
+/*   Updated: 2023/08/14 10:28:00 by rgallego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ void	ft_exit(char **args)
 	printf("exit\n");
 	if (1 < len_char_double_ptr(args) && !ft_isstrnbr(args[1]))
 	{
-		mini_error(NULL, args[CMD], NUMERIC_ARGS_ERR, NULL);
-		mini_error(args[ARG_1], NUMERIC_ARGS_MSG, NUMERIC_ARGS_ERR, NULL);
+		ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+		ft_putstr_fd(args[ARG_1], STDERR_FILENO);
+		ft_putendl_fd(NUMERIC_ARGS_MSG, STDERR_FILENO);
+		errno = NUMERIC_ARGS_ERR;
 	}
 	else if (2 < len_char_double_ptr(args))
 		mini_error(args[CMD], TOO_MANY_ARGS_MSG, TOO_MANY_ARGS_ERR, NULL);
